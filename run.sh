b9eb6de6aa96e44a0545528e92739b59891b508f
ronyfsw <rony@foresight.works>
2022-02-08 15:05:17 +0200

    deploy

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/rnd/Work/Analysis/Graphs_code
+echo `pwd`
+kill -9 `ps -eaf | grep cluster_service | grep -v grep | awk '{print $2}'`
+
+python cluster_service.py 2>&1 >> web_api.log &
+
+exit 0
