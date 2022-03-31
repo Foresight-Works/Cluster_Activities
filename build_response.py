14d772e431c7627c8360c2924268c968af5891a8
ronyfsw <rony@foresight.works>
2022-03-31 20:19:36 +0300

    naming by parts on researchUI

--- build_response.py
+++ build_response.py
@@ -1,41 +1,125 @@
-from setup import *
+import ast
+import os
+import re
+import nltk
+import itertools
+from itertools import combinations
+from nltk.corpus import stopwords
+import numpy as np
+import pandas as pd
+import mysql.connector as mysql
+from collections import defaultdict
+import string
 
-# Reference directory path
-print('Building response')
-experiment_id, best_run_id, file_names_str = sys.argv[1], sys.argv[2], sys.argv[3]
+db_name = 'CAdb'
+location_db_params = {'Local': {'host': 'localhost', 'user':'rony', 'password':'exp8546$fs', 'database': db_name},\
+                      'Remote': {'host': '172.31.36.11', 'user':'researchUIuser', 'password':'query1234$fs', 'database': db_name}}
+conn_params = location_db_params['Local']
+conn = mysql.connect(**conn_params)
+cur = conn.cursor()
+cur.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED")
+experiment_id = 10
+def result_from_table(experiment_id, result_key='clusters'):
+    result_df = pd.read_sql_query("SELECT * FROM results \
+    WHERE experiment_id={eid}".format(eid=experiment_id), conn)
+    result = result_df['result'].values[0]
+    result = ast.literal_eval(result)
+    if result_key == 'clusters':
+        result_key = [c for c in result.keys() if 'duration' not in c][0]
+    result = result[result_key]
+    clusters = {}
+    for k, v in result.items():
+        if len(k.split(' ')) > 1:
+            v = [i[1] for i in v]
+            clusters[k] = v
+    return clusters
 
-print('experiment_id:', experiment_id)
-print('best_run_id:', best_run_id)
-print('file_names_str in build response:', file_names_str)
-experiment_dir_name = 'experiment_{id}'.format(id=experiment_id)
-experiment_dir = os.path.join(results_dir, experiment_dir_name)
-run_dir = os.path.join(experiment_dir, 'runs', best_run_id)
-references_dir = os.path.join(run_dir, 'references')
-print('reference dictionaries directory:', references_dir)
-
-# Tokens distance matrices
+matrices_dir = '/home/rony/Projects_Code/Cluster_Activities/matrices'
 distance_matrices = []
 matrices = os.listdir(matrices_dir)
 for matrix in matrices:
     path = os.path.join(matrices_dir, matrix)
     distance_matrices.append(pd.read_pickle(path))
 
-# Experiment result (to build as response)
-clustering_result = {}
-if 'clustering_result.npy' in os.listdir(references_dir):
-    clustering_result = np.load(os.path.join(references_dir, 'clustering_result.npy'), allow_pickle=True)[()]
-    print('clustering_result example:', list(clustering_result.items())[:1])
+punctuation_marks="=|\+|_|\.|:|\/|\*|\'|,|\?"
+def split_tokens (tokens, splitter):
+    tokens_splitter= [t for t in tokens if splitter in t]
+    tokens = [t for t in tokens if splitter not in t]
+    for t in tokens_splitter: tokens += t.split(splitter)
+    return tokens
 
-# Reference dictionaries
-names_tokens, tokens_pairs_scores, clusters_names_pairs = {}, {}, {}
-if 'names_tokens.npy' in os.listdir(references_dir):
-    names_tokens = np.load(os.path.join(references_dir, 'names_tokens.npy'), allow_pickle=True)[()]
-if 'tokens_pairs_scores.npy' in os.listdir(references_dir):
-    tokens_pairs_scores = np.load(os.path.join(references_dir, 'tokens_pairs_scores.npy'), allow_pickle=True)[()]
-if 'clusters_names_pairs.npy' in os.listdir(references_dir):
-    clusters_names_pairs = np.load(os.path.join(references_dir, 'clusters_names_pairs.npy'), allow_pickle=True)[()]
-if 'clusters_namesIDs.npy' in os.listdir(run_dir):
-    clusters_namesIDs = np.load(os.path.join(run_dir, 'clusters_namesIDs.npy'), allow_pickle=True)[()]
+def isfloat(value):
+    '''
+    Check if the input value type is float
+    '''
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+def isint(value):
+    '''
+    Check if the input value type is integer
+    '''
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+def normalize_entities(name, punctuation_symbols=punctuation_marks):
+    '''
+    Identify names in tokens by the presence of symbols
+    '''
+    #print('normalize f')
+    #print('name:', name)
+    name = name.replace('&amp','')
+    tokens = name.split(' ')
+    for token in tokens:
+        if re.findall('\d', token):
+            if re.findall('[A-Za-z]', token):
+                name = name.replace(token, '<name>')
+            else:
+                name = name.replace(token, '<number')
+        elif re.findall(punctuation_symbols, token):
+            name = name.replace(token, '<name>')
+    name = name.replace('<name> <name>', '<name>').replace('<number> <number>', '<number>')
+
+    return name
+
+def tokenize(data, unique=False, is_list=False, exclude_stopwords=False, exclude_chars=True,\
+              split_backslah=True, split_hyphen=True, split_plus=True,\
+              clean_punctuation=False, exclude_numbers=False, exclude_digit_tokens=False, \
+              punctuation_symbols=punctuation_marks, stopwords=set(stopwords.words('english')),\
+             normalized_entities=True):
+
+    if is_list:
+        data = [t for t in data if type(t)==str]
+        data = ' '.join(data)
+        data = re.sub('\s{2,}', ' ', data)
+
+    if normalized_entities:
+        data = normalize_entities(data)
+        pattern = '\<.+?\>|\w*\d{1,}\.*\d{1,}\w*|\w+'
+        tokenizer = nltk.RegexpTokenizer(pattern)
+        tokens = tokenizer.tokenize(data)
+    else:
+        tokens = nltk.word_tokenize(data)
+    tokens = [t.lower() for t in tokens]
+    if split_backslah: tokens = split_tokens (tokens, '/')
+    if split_hyphen: tokens = split_tokens(tokens, '-')
+    if split_plus: tokens = split_tokens(tokens, '+')
+
+    if exclude_stopwords: tokens = [t for t in tokens if t not in stopwords]
+    if clean_punctuation: tokens = [re.sub(punctuation_symbols, '', t) for t in tokens]
+    if exclude_chars:tokens = [t for t in tokens if len(t) > 1]
+    if exclude_numbers:
+        tokens = [t for t in tokens if (not(isint(t)))]
+        tokens = [t for t in tokens if (not(isfloat(t)))]
+    if exclude_digit_tokens: tokens = [t for t in tokens if not re.findall('\d', t)]
+    # Unique tokens preserving the tokens order in the input text
+    if unique: tokens = sorted(set(tokens), key=tokens.index)
+    return tokens
 
 def tokens_count(tokens):
     counts = dict()
@@ -46,8 +130,14 @@ def tokens_count(tokens):
             counts[token] = 1
     return counts
 
-def get_cluster_key(cluster_id, cutoff=0.8):
-    cluster_names_pairs = clusters_names_pairs[cluster_id]
+def text_to_key(cluster_names, cutoff=0.8):
+    names_tokens = {}
+    for name in cluster_names:
+        tokens = tokenize(name, unique=True, exclude_stopwords=False, \
+                           exclude_numbers=True, exclude_digit_tokens=True)
+        names_tokens[name] = tokens
+    #print('names_tokens:', names_tokens)
+    cluster_names_pairs = tuple(combinations(cluster_names, 2))
     pairs_matches = []
     for name_pair in cluster_names_pairs:
         name1, name2 = name_pair
@@ -112,26 +202,70 @@ def get_cluster_key(cluster_id, cutoff=0.8):
     for pair_matches, match_score in match_scores.items():
         if match_score == max_score:
             cluster_key = pair_matches
-
     cluster_key = ' '.join(list(set(cluster_key)))
-    return cluster_id, cluster_key
+    return cluster_key
 
-def key_clusters(clustering_result, num_executors):
-    executor = ProcessPoolExecutor(num_executors)
-    cluster_ids = list(clustering_result.keys())
-    print('{n} cluster_ids:'.format(n=len(cluster_ids)), cluster_ids)
-    named_clusters, named_clusters_ids = {}, {}
-    for cluster_id, cluster_key in executor.map(get_cluster_key, cluster_ids):
-        activities_ids = clusters_namesIDs[cluster_id]
-        named_clusters[cluster_key] = list(zip(activities_ids, clustering_result[cluster_id]))
-        named_clusters[cluster_key] = [tuple(i) for i in named_clusters[cluster_key]]
-        named_clusters_ids[cluster_key] = activities_ids
-    executor.shutdown()
-    # Todo integration: replace file_names_str by the identifier(s) defined for the response
-    named_clusters = {file_names_str: named_clusters}
-    return named_clusters, named_clusters_ids
+def get_tokens_locations(parts):
+    tokens_locations = defaultdict(list)
+    for part in parts:
+        tokens = tokenize(part, unique=True, exclude_stopwords=False, \
+                          exclude_numbers=True, exclude_digit_tokens=True)
+        tokens_indices = [tokens.index(t) for t in tokens]
+        for token in tokens:
+            tokens_locations[token].append(tokens_indices[tokens.index(token)])
+    tokens_typical_locations = {}
+    for token, locations in tokens_locations.items():
+        token_typical_location = max(set(locations), key=locations.count)
+        tokens_typical_locations[token] = token_typical_location
 
-num_executors = int(config.get('run', 'num_executors'))
-named_clusters, named_clusters_ids = key_clusters(clustering_result, num_executors)
-np.save(os.path.join(results_dir, 'named_clusters.npy'), named_clusters)
-np.save(os.path.join(results_dir, 'named_clusters_ids.npy'), named_clusters_ids)
+    return tokens_typical_locations
+
+def get_key(names):
+    '''
+    Split a group of using a splitter symbol (e.g. hyphen) to produce lists of the phrase parts
+    Splitter: ' - '
+    '''
+    # Store names parts by their location relative to a hyphen break in each name
+    names_parts = defaultdict(list)
+    for name in names:
+        name_split = name.split(' - ')
+        delimiters = ' - |/|\(|\)|\[|\]' # To keep parenthesis use ' - |/|,(\(.+?\))'
+        name_split = [i.rstrip().lstrip() for i in re.split(delimiters, name) if i]
+
+        # Number of parts produced by a hyphen break
+        num_parts = len(name_split)
+        parts_indices = np.arange(num_parts)
+        for index in parts_indices:
+            names_parts[index].append(name_split[index])
+    names_parts = dict(names_parts)
+
+    key_parts = ['']
+    for index, names_part in names_parts.items():
+        if len(names_part) > 1:
+            # Get key by the name part
+            parts_key = get_key(names_part, cutoff=0.8)
+            part_key_tokens = tokenize(parts_key, unique=True, exclude_stopwords=False, \
+                                       exclude_numbers=True, exclude_digit_tokens=True)
+            # Re-order the key words by their typical order in the name parts
+            tokens_typical_locations = get_tokens_locations(names_part)
+            key_tokens_locations = {k: v for k, v in tokens_typical_locations.items() if k in part_key_tokens}
+            sorted_key_tokens_locations = {k: v for k, v in sorted(key_tokens_locations.items(), key=lambda item: item[1])}
+            parts_key = ' '.join(list(sorted_key_tokens_locations.keys()))
+            parts_key = string.capwords(parts_key)
+            key_parts.append(parts_key)
+
+    key_parts = [i for i in key_parts if i]
+    return ' - '.join(key_parts).lstrip(' - ')
+
+clusters = result_from_table(experiment_id)
+for key, names in clusters.items():
+    print('====== Cluster Names ========')
+    for name in names: print(name)
+    print('------ Keys Identified ------')
+    print('key (v1):', key)
+    new_key = text_to_key(names, cutoff=0.8)
+    print('key (v2):', new_key)
+    new_key = get_key(names)
+    print('key (v3):', new_key)
+
+
